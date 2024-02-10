095ce9d16cd73f7a4fb94a555c9a9f773dd0473c
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-11 00:20:02 +0100

    Show User's avatar when signed in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_avatar_url(user, size: nil)
+    return user.avatar_url if size.nil?
+
+    uri = URI.parse(user.avatar_url)
+    new_query_ar = URI.decode_www_form(uri.query || '') << ["s", size]
+    uri.query = URI.encode_www_form(new_query_ar)
+
+    uri.to_s
+  end
 end
