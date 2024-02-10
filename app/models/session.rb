42fd84f666f45de9d3fd51fd08899e061b6eb595
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-10 22:47:07 +0100

    Try to fix curious error with this find_by_encoded_id
    ArgumentError (wrong number of arguments (given 1, expected 0))

--- app/models/session.rb
+++ app/models/session.rb
@@ -6,6 +6,9 @@ class Session < ApplicationRecord
   validates :user_agent, presence: true
   validates :ip_address, presence: true
 
-  def self.find_by_encoded_id(encoded_id) = find_by_id(encoded_id.to_i(36))
+  def self.find_by_encoded_id(identifier)
+    find_by_id(identifier.to_i(36))
+  end
+
   def encoded_id = id.to_s(36).rjust(10, "0")
 end
