cea95aee1be2013880398582d1e386c32746d944
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-10 22:10:00 +0100

    Fix the GitHub authorization controller

--- app/controllers/github/authorizations_controller.rb
+++ app/controllers/github/authorizations_controller.rb
@@ -7,7 +7,7 @@ module GitHub
     # see: https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#1-request-a-users-github-identity
     AUTHORIZE_URL = URI("https://github.com/login/oauth/authorize")
     # see: https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/scopes-for-oauth-apps
-    SCOPE = "read:user user:email"
+    SCOPE = "read:user"
     # see: https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
     TOKEN_URL = URI("https://github.com/login/oauth/access_token")
     # see: https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#3-use-the-access-token-to-access-the-api
@@ -18,25 +18,32 @@ module GitHub
     private
 
       def authorization_succeeded(auth_info)
-        if (connected_account = User::ConnectedAccount.for_auth(auth_info)).present?
-          connected_account.update(connected_account_params)
-          sign_in(user: connected_account.user)
+        if (user = User.find_by(github_uid: auth_info["uid"])).present?
+          user.update(
+            avatar_url: auth_info.dig("info", "image"),
+            github_username: auth_info.dig("info", "login"),
+            twitter_username: auth_info.dig("info", "twitter_username")
+          )
+          sign_in(user:)
           redirect_to after_oauth_path,
             notice: "Successfully updated GitHub account"
 
         elsif Current.user.present?
-          Current.user.connected_accounts.create(connected_account_params)
+          Current.user.update(
+            avatar_url: auth_info.dig("info", "image"),
+            github_username: auth_info.dig("info", "login"),
+            twitter_username: auth_info.dig("info", "twitter_username")
+          )
           redirect_to after_oauth_path,
             notice: "Successfully connected GitHub account"
 
         else
           user = User.new(
-            screen_name: auth_info.dig(:info, :login) ||
-                         auth_info.dig(:info, :email).split("@").first,
-            full_name: auth_info.dig(:info, :name),
-            email: auth_info.dig(:info, :email)
+            github_uid: auth_info["uid"],
+            avatar_url: auth_info.dig("info", "image"),
+            github_username: auth_info.dig("info", "login"),
+            twitter_username: auth_info.dig("info", "twitter_username")
           )
-          user.connected_accounts.new(connected_account_params)
 
           if user.save
             sign_in(user:)
@@ -44,14 +51,14 @@ module GitHub
               notice: "Signed in successfully via GitHub"
           else
             redirect_to new_session_path,
-              alert: error_alert
+              alert: "Authentication with GitHub failed: invalid user details"
           end
         end
       end
 
       def authorization_failed
         redirect_to new_session_path,
-          alert: error_alert
+          alert: "Authentication with GitHub failed"
       end
   end
 end
