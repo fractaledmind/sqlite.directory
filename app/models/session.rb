c712c389dfbc5c69f764ff1ece55aa0fe7a77ca7
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-10 22:33:36 +0100

    Expose an encoded_id for Session

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,4 +5,7 @@ class Session < ApplicationRecord
 
   validates :user_agent, presence: true
   validates :ip_address, presence: true
+
+  def self.find_by_encoded_id(encoded_id) = find_by_id(encoded_id.to_i(36))
+  def encoded_id = id.to_s(36).rjust(10, "0")
 end
