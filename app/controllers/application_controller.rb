0dd8232b7bf48f36fb0043431f21ab39252b0484
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-10 21:46:35 +0100

    Add method to sign in a User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
+
+  def sign_in(user:)
+    session = user.sessions.create!(
+      user_agent: request.user_agent,
+      ip_address: request.ip
+    )
+    Current.session = session
+    cookies.signed.permanent[Session::COOKIE_KEY] = {
+      value: session.encoded_id,
+      httponly: true,
+      secure: !Rails.env.development?
+    }
+    true
+  end
 end
