95df15f790b4237960eca119e86ab07a94d2c965
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-10 22:18:24 +0100

    Add the User -> Session association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :sessions, dependent: :destroy
+
   validates :github_uid, presence: true, uniqueness: true
   validates :github_username, presence: true
 end
