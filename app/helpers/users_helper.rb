06baaf63d0f6a958ef9df36da6e3d3a74e3bd24d
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-11 12:13:14 +0100

    Fix Rubocop issues

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     return user.avatar_url if size.nil?
 
     uri = URI.parse(user.avatar_url)
-    new_query_ar = URI.decode_www_form(uri.query || '') << ["s", size]
+    new_query_ar = URI.decode_www_form(uri.query || "") << [ "s", size ]
     uri.query = URI.encode_www_form(new_query_ar)
 
     uri.to_s
