dbfe908e3ed1a42803857e8b7e550d28ff7c19b1
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-11 14:42:03 +0100

    Simplify the entry_submitted_by helper

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,26 +2,23 @@ module EntriesHelper
   def entry_submitted_by(user)
     tag.span(class: "inline-flex items-center gap-1") do
       safe_join([
-        tag.span("By: "),
         image_tag(user_avatar_url(user, size: 32), class: "w-4 h-4 rounded-full"),
-        tag.strong do
-          if user.twitter_username.present?
-            link_to "https://x.com/#{user.twitter_username}", class: "group" do
-              safe_join([
-                tag.i("@"),
-                tag.span(user.twitter_username, class: "group-hover:underline")
-              ])
-            end
-          elsif user.github_username.present?
-            link_to "https://github.com/#{user.github_username}", class: "group" do
-              safe_join([
-                tag.i("@"),
-                tag.span(user.github_username, class: "group-hover:underline")
-              ])
-            end
-          else
-            user.name
+        if user.twitter_username.present?
+          link_to "https://x.com/#{user.twitter_username}", class: "group" do
+            safe_join([
+              tag.i("@"),
+              tag.span(user.twitter_username, class: "group-hover:underline")
+            ])
           end
+        elsif user.github_username.present?
+          link_to "https://github.com/#{user.github_username}", class: "group" do
+            safe_join([
+              tag.i("@"),
+              tag.span(user.github_username, class: "group-hover:underline")
+            ])
+          end
+        else
+          user.name
         end
       ])
     end
