f7b85ece19a4b34b2e787f3b6163624c7f2c48a4
Stephen Margheim <stephen.margheim@gmail.com>
2024-12-08 23:40:38 +0100

    Add basic light dismiss functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+const dismissableElements = document.querySelectorAll("[dismissable]")
+document.addEventListener('click', event => {
+  dismissableElements.forEach(element => {
+    if (element === event.target || element.contains(event.target)) return;
+
+    element.open = false
+  })
+})
