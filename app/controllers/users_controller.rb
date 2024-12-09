8bb48344488a82fbccb44f7d199831c69c5e0709
Stephen Margheim <stephen.margheim@gmail.com>
2024-12-09 01:41:10 +0100

    Fix users controller comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :ensure_user_authenticated!, only: %i[show]
   before_action :user_authenticated?, only: %i[show]
 
-  # GET /users/:id
+  # GET /users/:slug
   def show
     @user = User.find_by!(github_username: params[:slug])
   end
